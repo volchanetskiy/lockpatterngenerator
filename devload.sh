fe7f81bb2e246e7117e86c06620257fa6ed98a1c
Michael Shick <android@shick.in>
2011-08-14 23:15:38 -0400

    cleaned up git repo for portability

--- devload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PNAME='in.shick.lockpatterngenerator'
-TITLE='LockPatternGenerator'
-
-adb -d uninstall ${PNAME}
-adb -d install bin/${TITLE}-debug.apk
--- emuload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PNAME='in.shick.lockpatterngenerator'
-TITLE='LockPatternGenerator'
-
-adb -e uninstall ${PNAME}
-adb -e install bin/${TITLE}-debug.apk
